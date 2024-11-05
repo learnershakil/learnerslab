4e73dfc7955518cea74e0154472e582850fde0dc
SHAKIL AHMAD <learnershakil@gmail.com>
2024-11-05 20:39:08 +0530

    feat(navbar): add Navbar for Desktop & Mobile

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-center pt-10 font-bold text-4xl">
+                Welcome to Learner&apos;s Lab
+            </h1>
+        </>
+    )
+}
